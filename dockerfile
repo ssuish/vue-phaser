f99518c952c5328b5fc2cfc21488ceff51a219c1
suisoh <fubuki.k016@gmail.com>
2023-09-25 19:28:57 +0800

    added basic function

--- dockerfile
+++
@@ -1,5 +0,0 @@
-RUN npm i -g vue-cli \
-npm i -g @ionic/cli \
-ionic start phaserGame tabs --type vue \
-cd phaserGame \ 
-npm i phaser
---
+++ phaserGame/src/components/PhaserContainer.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="game">
+        <ion-button v-if="showButton" @click="handleClickStart">Start</ion-button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { IonButton } from '@ionic/vue';
+import { Game } from 'phaser';
+
+const showButton = ref(true)
+
+function launch() {
+
+}
+
+function handleClickStart() {
+    showButton.value = false
+    launch()
+}
+
+</script>
+
+<style lang="css" scoped>
+#game {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+}
+</style>
